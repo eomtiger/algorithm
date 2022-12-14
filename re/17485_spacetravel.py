10461acb3ded2d34d61202cbf5ed715f7a4b17c9
unknown <eomtiger@gmail.com>
2022-12-14 16:54:48 +0900

    221214

--- re/17485_spacetravel.py
+++ re/17485_spacetravel.py
@@ -1,48 +1,28 @@
 import sys
-sys.setrecursionlimit(10**6)
 input = sys.stdin.readline
-
-def move(direction, value_sum, stage, j):
-
-    global minV
-    # if value_sum < check[stage][j]:
-    #     check[stage][j] = value_sum
-    # else:
-    #     return
-    check[stage][j] = value_sum
-
-    if value_sum >= minV:
-        return
-
-    if stage == N-1:
-        if value_sum < minV:
-            minV = value_sum
-        return
-
-    dir = [0, 1, 2]
-    if stage != 0:
-        dir.remove(direction)
-
-    for i in dir:
-        if i == 0 and 0 <= j-1 < M and check[stage+1][j-1] > value_sum + mat[stage+1][j-1]:    #왼쪽 아래
-            move(i, value_sum + mat[stage+1][j-1], stage+1, j-1)
-
-        if i == 1 and check[stage+1][j] > value_sum + mat[stage+1][j]:                      #바로 아래
-            move(i, value_sum + mat[stage+1][j], stage+1, j)
-
-        if i == 2 and 0 <= j+1 < M and check[stage+1][j+1] > value_sum + mat[stage+1][j+1]:    #오른쪽 아래
-            move(i, value_sum + mat[stage+1][j+1], stage+1, j+1)
-
-
+INF = sys.maxsize
 N, M = map(int, input().split())
+board = [[INF] + list(map(int, input().split())) + [INF] for _ in range(N)]
 
-minV = 987654321
-mat = [list(map(int, input().split())) for _ in range(N)]
+print(board)
 
-check = [[987654321] * M for _ in range(N)]
-# print(check)
-for j in range(len(mat[0])):
-    move(0, mat[0][j], 0, j)  #내려온 방향, 지금까지 비용, 층, j 좌표
+DP = [[(0, 0, 0) for i in range(M+2)] for j in range(N)]
+print(DP)
+for first_dp in range(1, M+1):
+    DP[1][first_dp] = (board[1][first_dp] + board[0][first_dp - 1],
+                       board[1][first_dp] + board[0][first_dp],
+                       board[1][first_dp] + board[0][first_dp + 1])
 
-# print(check)
-print(minV)
+print(DP)
+for x in range(2, N):
+    DP[x][1] = (INF,
+                board[x][1] + min(DP[x - 1][1][0], DP[x - 1][1][2]),
+                board[x][1] + min(DP[x - 1][2][0], DP[x - 1][2][1]))
+    DP[x][M] = (board[x][M] + min(DP[x - 1][M - 1][1], DP[x - 1][M - 1][2]),
+                board[x][M] + min(DP[x - 1][M][0], DP[x - 1][M][2]),
+                INF)
+    for y in range(2, M):
+        DP[x][y] = (board[x][y] + min(DP[x - 1][y - 1][1], DP[x - 1][y - 1][2]),
+                    board[x][y] + min(DP[x - 1][y][0], DP[x - 1][y][2]),
+                    board[x][y] + min(DP[x - 1][y + 1][0], DP[x - 1][y + 1][1]))
+print(min(map(min, DP[N-1][1:M+1])))
