7caf57ae3a3ea29bd03578744fde7258c92e56f7
엄희원 <eom9300@hanmail.net>
2023-02-21 22:16:12 +0900

    1149

---
+++ 1149_RGB거리.py
@@ -0,0 +1,29 @@
+N = int(input())
+
+mat = [list(map(int, input().split())) for _ in range(N)]
+
+# print(mat)
+
+dp = [[0, 0, 0] for i in range(N)]
+
+# print(dp)
+
+for i in range(N):
+    for j in range(3):
+        if i == 0:
+            dp[i][j] = mat[i][j]
+
+        else:
+            if j == 0:
+                dp[i][j] = min(mat[i][j] + dp[i-1][j+1], mat[i][j] + dp[i-1][j+2])
+
+            elif j == 1:
+                dp[i][j] = min(mat[i][j] + dp[i-1][j-1], mat[i][j] + dp[i-1][j+1])
+
+            else:
+                dp[i][j] = min(mat[i][j] + dp[i-1][j-2], mat[i][j] + dp[i-1][j-1])
+
+print(min(dp[-1]))
+                
+
+
